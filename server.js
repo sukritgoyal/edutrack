b472e2ffc17e848aad21e55787a593f53f68b258
sukritgoyal <68213552+sukritgoyal@users.noreply.github.com>
2024-04-24 22:42:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,4 @@
 import app from "./App.js";
-import cloudinary from "cloudinary";
-
-cloudinary.v2.config({
-	cloud_name: process.env.CLOUD_NAME,
-	api_key: process.env.API_KEY,
-	api_secret: process.env.API_SECRET,
-});
 
 app.listen(process.env.PORT, () => {
 	console.log(`SERVER RUNNING ON ${process.env.PORT}`);
